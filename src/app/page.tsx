fcf535fd6a09e3a37fe0f3926b2c0371d6dd5d8b
EddieZhang540 <55262779+EddieZhang540@users.noreply.github.com>
2025-01-21 16:46:28 -0500

    connect to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 // app/page.tsx
-import React from 'react';
+"use client";
+import React, { useState } from "react";
 
 export default function HomePage() {
+  const [response, setResponse] = useState();
+  const fetchData = async () => {
+    const response = await fetch("/api");
+    const data = await response.json();
+    setResponse(data);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
       <div className="max-w-4xl text-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        {/* <h1 className="text-4xl font-bold text-gray-800 mb-4">
           Welcome to Your Next.js App! 🚀
         </h1>
         <p className="text-lg text-gray-600 mb-6">
@@ -28,7 +36,11 @@ export default function HomePage() {
           >
             GitHub Repository
           </a>
-        </div>
+        </div> */}
+        <button className='bg-gray-800 text-white px-4 py-2 rounded shadow hover:bg-gray-700' onClick={fetchData}>
+          Get all students
+        </button>
+        {JSON.stringify(response)}
       </div>
     </main>
   );
