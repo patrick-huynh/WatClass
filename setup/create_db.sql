e9c917285a77ca2d8931a65e61c464872a93beeb
EddieZhang540 <55262779+EddieZhang540@users.noreply.github.com>
2025-02-06 12:38:29 -0500

    Update create_db.sql

--- setup/create_db.sql
+++ setup/create_db.sql
@@ -17,29 +17,29 @@ DROP TABLE IF EXISTS Questions;
 DROP TABLE IF EXISTS Courses;
 
 CREATE TABLE Courses (
-  courseId VARCHAR(20) PRIMARY KEY,
-  courseName VARCHAR(255) NOT NULL,
-  courseSubject VARCHAR(100) NOT NULL
+  cId VARCHAR(20) PRIMARY KEY, -- course code
+  name VARCHAR(255) NOT NULL,
+  subject VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE Questions (
-  questionId INT PRIMARY KEY AUTO_INCREMENT,
-  question TEXT NOT NULL
+  qId INT PRIMARY KEY AUTO_INCREMENT,
+  questionText VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE CourseRatings (
-  courseId VARCHAR(20) PRIMARY KEY,
+  cId VARCHAR(20) PRIMARY KEY,
   analyticalThinking INT CHECK (analyticalThinking BETWEEN 1 AND 5),
   creativity INT CHECK (creativity BETWEEN 1 AND 5),
   collaboration INT CHECK (collaboration BETWEEN 1 AND 5),
   trivia INT CHECK (trivia BETWEEN 1 AND 5),
   difficulty INT CHECK (difficulty BETWEEN 1 AND 5),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId)
+  FOREIGN KEY (cId) REFERENCES Courses (cId)
 );
 
 CREATE TABLE Users (
-  userId INT PRIMARY KEY AUTO_INCREMENT,
-  userName VARCHAR(255) NOT NULL,
+  uId INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
   term VARCHAR(3) CHECK (
     term IN ('1A', '1B', '2A', '2B', '3A', '3B', '4A', '4B')
   ),
@@ -47,45 +47,45 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE NextQuestionLookup (
-  questionId INT,
+  qId INT,
   expectedAnswer INT CHECK (expectedAnswer BETWEEN 1 AND 10),
   nextQuestion INT,
-  PRIMARY KEY (questionId, expectedAnswer),
-  FOREIGN KEY (questionId) REFERENCES Questions (questionId),
-  FOREIGN KEY (nextQuestion) REFERENCES Questions (questionId)
+  PRIMARY KEY (qId, expectedAnswer),
+  FOREIGN KEY (qId) REFERENCES Questions (qId),
+  FOREIGN KEY (nextQuestion) REFERENCES Questions (qId)
 );
 
 CREATE TABLE Answers (
-  userId INT,
-  questionId INT,
+  uId INT,
+  qId INT,
   userAnswer INT CHECK (userAnswer BETWEEN 1 AND 10),
-  PRIMARY KEY (userId, questionId),
-  FOREIGN KEY (userId) REFERENCES Users (userId),
-  FOREIGN KEY (questionId) REFERENCES Questions (questionId)
+  PRIMARY KEY (uId, qId),
+  FOREIGN KEY (uId) REFERENCES Users (uId),
+  FOREIGN KEY (qId) REFERENCES Questions (qId)
 );
 
 CREATE TABLE CourseTaken (
-  courseId VARCHAR(20),
-  userId INT,
-  PRIMARY KEY (courseId, userId),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId),
-  FOREIGN KEY (userId) REFERENCES Users (userId)
+  uId INT,
+  cId VARCHAR(20),
+  PRIMARY KEY (uId, cId),
+  FOREIGN KEY (uId) REFERENCES Users (uId),
+  FOREIGN KEY (cId) REFERENCES Courses (cId)
 );
 
 CREATE TABLE CourseWishlist (
-  courseId VARCHAR(20),
-  userId INT,
-  PRIMARY KEY (courseId, userId),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId),
-  FOREIGN KEY (userId) REFERENCES Users (userId)
+  uId INT,
+  cId VARCHAR(20),
+  PRIMARY KEY (uId, cId),
+  FOREIGN KEY (uId) REFERENCES Users (uId),
+  FOREIGN KEY (cId) REFERENCES Courses (cId)
 );
 
 CREATE TABLE CourseRecommended (
-  courseId VARCHAR(20),
-  userId INT,
-  PRIMARY KEY (courseId, userId),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId),
-  FOREIGN KEY (userId) REFERENCES Users (userId)
+  uId INT,
+  cId VARCHAR(20),
+  PRIMARY KEY (uId, cId),
+  FOREIGN KEY (uId) REFERENCES Users (uId),
+  FOREIGN KEY (cId) REFERENCES Courses (cId)
 );
 
 -- Load sample data
