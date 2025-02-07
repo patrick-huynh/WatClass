d1a23a19b20a03e12307868f3486267daa09c133
EddieZhang540 <55262779+EddieZhang540@users.noreply.github.com>
2025-02-07 13:18:39 -0500

    fix sample name

--- milestone-1/test-sample-R10.sql
+++ milestone-1/test-sample-R10.sql
@@ -1,8 +1,4 @@
-SELECT c.subject, 
-    AVG(r.difficulty), 
-    AVG(r.analyticalThinking), 
-    AVG(r.creativity), 
-    AVG(r.collaboration) 
-    FROM 
-    (CourseRatings r JOIN Courses c on r.cId = c.cId) 
-    GROUP BY c.subject;
+SELECT
+  *
+FROM
+  Courses;
--- milestone-1/test-sample-R6.sql
+++ milestone-1/test-sample-R6.sql
@@ -1,4 +1,8 @@
-SELECT
-  *
-FROM
-  Courses;
+SELECT c.subject, 
+    AVG(r.difficulty), 
+    AVG(r.analyticalThinking), 
+    AVG(r.creativity), 
+    AVG(r.collaboration) 
+    FROM 
+    (CourseRatings r JOIN Courses c on r.cId = c.cId) 
+    GROUP BY c.subject;
