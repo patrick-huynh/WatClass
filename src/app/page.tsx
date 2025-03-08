edb412bed82aeedd552f5935fa7e23eb184ba29e
aadar <aadar100@gmail.com>
2025-03-08 14:56:28 -0500

    Added a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoursesList from "./components/CoursesList";
 import AggregateStats from "./components/AggregateStats";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <Image src="/Logo.svg" alt="logo" width={200} height={200} />
       <div className="max-w-4xl text-center">
         <h1 className="text-2xl font-bold mb-4">Course and Stats Viewer</h1>
         <CoursesList />
