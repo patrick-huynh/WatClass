2e690c23df540c656075fc9c4f6feae7b7980504
EddieZhang540 <55262779+EddieZhang540@users.noreply.github.com>
2025-03-12 17:51:43 -0400

    load test user

--- setup/migrate_db_milestone2.sql
+++
@@ -1,7 +0,0 @@
--- Run this script to update db if it was setup using create_db from milestone 1
-USE WatClass;
-
-ALTER TABLE
-  CourseWishlist RENAME TO CoursePinned;
-
-DROP TABLE IF EXISTS CourseTaken;
