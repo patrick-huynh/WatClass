e0dc897a17a770a45bcb7de1a60047f1159090ad
Patrick Huynh <patrick.huynh@uwaterloo.ca>
2025-03-26 21:39:26 -0400

    update route associated with R6 to use VIEWS

--- src/app/api/aggregate/route.ts
+++ src/app/api/aggregate/route.ts
@@ -4,14 +4,13 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   try {
     const connection = await createConnection();
-    const sqlQuery = `SELECT c.subject, 
-    AVG(r.difficulty), 
-    AVG(r.analyticalThinking), 
-    AVG(r.creativity), 
-    AVG(r.collaboration) 
-    FROM 
-    (CourseRatings r JOIN Courses c on r.cId = c.cId) 
-    GROUP BY c.subject;`;
+    const sqlQuery = `SELECT subject, 
+    AVG(difficulty), 
+    AVG(analyticalThinking), 
+    AVG(creativity), 
+    AVG(collaboration) 
+    FROM CourseRatingView
+    GROUP BY subject;`
     const [rows] = await connection.query(sqlQuery);
     return NextResponse.json(rows);
   } catch (error) {
