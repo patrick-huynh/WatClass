7c8b30d6e30885a2abe29f4e6a6ce3b98f9ef2dd
Edward Wang <edwardw483@gmail.com>
2025-03-28 16:40:02 -0400

    use transaction for atomic commits

--- src/app/api/add_course/route.ts
+++ src/app/api/add_course/route.ts
@@ -21,8 +21,10 @@ export const POST = async (req: NextRequest) => {
       return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
     const insertCourses = `INSERT INTO Courses (cId, name, subject) VALUES (?, ?, ?);`;
     const insertRatings = `INSERT INTO CourseRatings (cId, analyticalThinking, creativity, collaboration, difficulty) VALUES (?, ?, ?, ?, ?)`;
+    await connection.beginTransaction()
     await connection.execute(insertCourses, [cId, name, subject]); 
     await connection.execute(insertRatings, [cId, analyticalThinking, creativity, collaboration, difficulty]);
+    await connection.commit()
     return NextResponse.json({ message: "Course added successfully"}, { status: 201 });
   } catch (error: any) {
     console.log(error);
